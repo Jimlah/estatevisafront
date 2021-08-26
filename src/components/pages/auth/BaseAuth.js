5c1b152ba02baa6ada71f741c34f56642f0a2ea4
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-08-26 12:50:45 +0100

    Modified BaseAuth

--- src/components/pages/auth/BaseAuth.js
+++ src/components/pages/auth/BaseAuth.js
@@ -1,9 +1,10 @@
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, useRouteMatch } from "react-router-dom";
 import register_bg_2 from "../../../assets/img/register_bg_2.png";
 import ForgotPassword from "./ForgotPassword";
 import Login from "./Login";
 
 const BaseAuth = () => {
+  const { path, url } = useRouteMatch();
   return (
     <div
       className="h-screen w-full bg-cover bg-no-repeat bg-gray-200 dark:bg-gray-900"
@@ -11,8 +12,8 @@ const BaseAuth = () => {
     >
       <div className="flex items-center justify-center h-screen px-10 overflow-hidden">
         <Switch>
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/forget-password" component={ForgotPassword} />
+          <Route path="/auth/login" component={Login} />
+          <Route path="/auth/forgot-password" component={ForgotPassword} />
         </Switch>
       </div>
     </div>
