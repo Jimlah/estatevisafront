b5801d80674fa65d1fb265f4b4c3a1907fb36e1b
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-08-26 11:21:39 +0100

    Created a submit button component

---
+++ src/components/form/SubmitButton.js
@@ -0,0 +1,14 @@
+const SubmitButton = () => {
+  return (
+    <div className="w-full">
+      <button
+        type="submit"
+        className="text=center w-full border py-2 rounded-md font-bold bg-transparent text-blue-500 border-blue-500 hover:bg-blue-500 hover:text-white"
+      >
+        Submit
+      </button>
+    </div>
+  );
+};
+
+export default SubmitButton;
