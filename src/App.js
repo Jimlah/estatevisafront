97eca8b44dc13ee37050236e220968dc6c164aff
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-08-26 12:50:20 +0100

    added message status

--- src/App.js
+++ src/App.js
@@ -2,21 +2,35 @@ import { AlertContext } from "./context/AlertContext";
 import { useState } from "react";
 import Alert from "./components/notifications/Alert";
 import BaseAuth from "./components/pages/auth/BaseAuth";
+import { UserContext } from "./context/UserContext";
+import Root from "./components/pages/layout/Root";
 
 const App = () => {
   const [message, setMessage] = useState(null);
   const [errors, setErrors] = useState(null);
+  const [user, setUser] = useState(null);
+  const [status, setStatus] = useState(null);
 
-  const initialAlertState = { message, errors, setErrors, setMessage };
+  const initialUserState = { user, setUser };
+  const initialAlertState = {
+    message,
+    errors,
+    setErrors,
+    setMessage,
+    status,
+    setStatus,
+  };
 
   return (
     <div>
-      <AlertContext.Provider value={initialAlertState}>
-        {message && (
-          <Alert message={message} handleClick={() => setMessage(null)} />
-        )}
-        <BaseAuth />
-      </AlertContext.Provider>
+      <UserContext.Provider value={initialUserState}>
+        <AlertContext.Provider value={initialAlertState}>
+          {message && (
+            <Alert message={message} status={status} handleClick={() => setMessage(null)} />
+          )}
+          <Root />
+        </AlertContext.Provider>
+      </UserContext.Provider>
     </div>
   );
 };
