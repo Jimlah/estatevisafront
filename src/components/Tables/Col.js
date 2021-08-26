a4ea6f108c9520891ae229c973143e673bae9f11
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-08-26 11:20:14 +0100

    Restructured the project folders

--- src/components/Tables/Col.js
+++
@@ -1,5 +0,0 @@
-const Col = ({ value }) => {
-  return <td className="px-6 py-2">{value}</td>;
-};
-
-export default Col;
--- src/utils/Fetch.js
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-
-export const fetch = (url, options) => {
-  
-
-  return {};
-};
-
-export default fetch;
