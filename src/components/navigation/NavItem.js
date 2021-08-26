c8626c073c2891eb67906f1c6b3f03116baa8fce
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-08-26 15:06:09 +0100

    Created the navigation item

---
+++ src/components/navigation/NavItem.js
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+import { MdDashboard } from "react-icons/md";
+import { IconContext } from "react-icons/lib";
+const NavItem = ({ to, name, icon, hide = false }) => {
+  return (
+    <Link
+      to={to}
+      className="group px-2 py-1 rounded-md text-xs font-medium tracking-wider text-gray-500 flex items-center justify-center space-x-3 hover:text-gray-900 capitalize"
+    >
+      <span>
+        <IconContext.Provider
+          value={{
+            className: "h-5 w-5 group-hover:text-indigo-500",
+          }}
+        >
+          {icon}
+        </IconContext.Provider>
+      </span>
+      {!hide && <span>{name}</span>}
+    </Link>
+  );
+};
+
+export default NavItem;
