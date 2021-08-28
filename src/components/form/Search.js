8e52228a8f03c323321d511002dd46972f95f347
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-08-28 09:25:32 +0100

    added search hooks to search to search input

--- src/components/form/Search.js
+++ src/components/form/Search.js
@@ -1,6 +1,18 @@
 import { AiOutlineSearch } from "react-icons/ai";
+import useSearch from "../../hooks/useSearch";
+import { useContext, useEffect } from "react";
+import { SearchContext } from "./../../context/SearchContext";
 
 const Search = () => {
+  const { searchData: data, setResult } = useContext(SearchContext);
+  const { search, searchData, handleSearch } = useSearch(data);
+
+  useEffect(() => {
+    setResult(searchData);
+
+    // eslint-disable-next-line
+  }, [searchData]);
+
   return (
     <div className="w-full flex flex-auto items-center justify-start border-b border-indigo-500 text-gray-500 space-x-0.5">
       <input
@@ -9,6 +21,8 @@ const Search = () => {
         id="search"
         placeholder="search"
         className="w-full focus:outline-none text-xs sm:text-sm capitalize bg-transparent"
+        value={search}
+        onChange={handleSearch}
       />
       <label htmlFor="search">
         <AiOutlineSearch />
