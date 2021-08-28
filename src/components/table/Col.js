5d74b4402a896d8dd9c7b32e06aca7ec9af63856
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-08-28 09:24:29 +0100

    modified Col component

--- src/components/table/Col.js
+++ src/components/table/Col.js
@@ -1,7 +1,8 @@
 import Cell from "./Cell";
 const Col = ({ children, head = false }) => {
-  const thead = "text-sm font-bold tracking-wide text-gray-500 uppercase";
-  const tbody = "text-xs font-medium dark:text-gray-200 capitalize";
+  const thead =
+    "text-sm font-bold tracking-wide text-gray-500 uppercase sticky top-0";
+  const tbody = "text-sm font-medium dark:text-gray-200 capitalize";
 
   return (
     <>
