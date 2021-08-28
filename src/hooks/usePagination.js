ba6816f954ee20f1e95f6149991951f1b3d1550c
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-08-28 15:00:20 +0100

    Created Pagination Hook

---
+++ src/hooks/usePagination.js
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+
+const usePagination = (data) => {
+  const [pageData, setPageData] = useState([]);
+  const [chunkedData, setChunkedData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const itemPerPage = 8;
+
+  const handlePrev = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const handleNext = () => {
+    if (currentPage < chunkedData?.length) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  useEffect(() => {
+    var ceil = Math.ceil;
+
+    /*eslint no-extend-native: ["error", { "exceptions": ["Array"] }]*/
+    Object.defineProperty(Array.prototype, "chunk", {
+      value: function (n) {
+        if (this.length <= n) {
+          return [this];
+        }
+        return Array(ceil(this.length / n))
+          .fill()
+          .map((_, i) => this.slice(i * n, i * n + n));
+      },
+      configurable: true,
+    });
+
+    setChunkedData(data?.chunk(itemPerPage));
+  }, [data, itemPerPage, currentPage]);
+
+  useEffect(() => {
+    if (chunkedData?.length > 0) {
+      setPageData(chunkedData[currentPage - 1]);
+    }
+  }, [chunkedData, currentPage]);
+
+  return { pageData, currentPage, handleNext, handlePrev, setCurrentPage };
+};
+
+export default usePagination;
