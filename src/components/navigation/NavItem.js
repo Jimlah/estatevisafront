17b71db9de2c72f95516e902de459483502f4262
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-08-29 22:28:27 +0100

    added isActive to the Navigation

--- src/components/navigation/NavItem.js
+++ src/components/navigation/NavItem.js
@@ -1,10 +1,21 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { IconContext } from "react-icons/lib";
+import { useEffect } from "react";
+import { useState } from "react";
 const NavItem = ({ to, name, icon, hide = false }) => {
+  const location = useLocation();
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    setIsActive(location.pathname === to);
+  }, [location]);
+
   return (
     <Link
       to={to}
-      className="group px-2 py-1 rounded-md text-xs font-medium tracking-wider text-gray-500 flex items-center justify-center space-x-3 hover:text-gray-900 capitalize"
+      className={`group px-2 py-1 rounded-md text-xs font-medium tracking-wider text-gray-500 flex items-center justify-start space-x-3 hover:text-gray-900 capitalize hover:bg-gray-200 dark:hover:bg-indigo-900 w-full ${
+        isActive ? "bg-gray-100 dark:bg-indigo-900 dark:text-white" : ""
+      }`}
     >
       <span>
         <IconContext.Provider
