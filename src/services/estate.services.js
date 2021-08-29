202bb38ae248b48c0223865c1696d9e1dd4bf220
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-08-29 02:03:58 +0100

    Added delete to the estate service

--- src/services/estate.services.js
+++ src/services/estate.services.js
@@ -1,16 +1,27 @@
-import http from './base.services';
+import http from "./base.services";
 const getAll = async () => {
-    
-    var resp = {}
-    try {
-        const response = await http.get('/estates');
-        resp = response.data
-    } catch (e) {
-        resp = e.resp.data??null;
-    }
-    return resp;
+  var resp = {};
+  try {
+    const response = await http.get("/estates");
+    resp = response.data;
+  } catch (e) {
+    resp = e.response.data ?? null;
+  }
+  return resp;
+};
+
+const destroy = async (id) => {
+  var resp = {};
+  try {
+    const response = await http.delete("/estates/" + id);
+    resp = response?.data;
+  } catch (e) {
+    resp = e.response?.data ?? null;
+  }
+  return resp;
 };
 
 export const Estate = {
-    getAll
-}
+  getAll,
+  destroy,
+};
