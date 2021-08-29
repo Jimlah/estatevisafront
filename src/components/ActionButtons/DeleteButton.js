1ac7d802537ba2b8621fa08564c2e1778aa15514
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-08-29 02:05:22 +0100

    created Delete button component

---
+++ src/components/ActionButtons/DeleteButton.js
@@ -0,0 +1,19 @@
+import { IconContext } from "react-icons/lib";
+import { RiDeleteBin6Line } from "react-icons/ri";
+
+const DeleteButton = ({ handleDelete }) => {
+  return (
+    <div className="">
+      <button
+        onClick={handleDelete}
+        className="text-red-500 focus:outline-none hover:text-red-700"
+      >
+        <IconContext.Provider value={{ className: "h-4 w-4" }}>
+          <RiDeleteBin6Line />
+        </IconContext.Provider>
+      </button>
+    </div>
+  );
+};
+
+export default DeleteButton;
