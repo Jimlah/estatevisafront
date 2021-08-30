f6601a9aa5a1f58bbce3d383f50f71497d6de801
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-08-30 10:58:29 +0100

    make the view Estate into component

--- src/components/pages/dashboard/Estate/ViewEstates.js
+++ src/components/pages/dashboard/Estate/ViewEstates.js
@@ -6,28 +6,6 @@ import DeleteButton from "./../../../ActionButtons/DeleteButton";
 import Logo from "../../../miscellaneous/Logo";
 import Table from "./../../../table/Table";
 const ViewEstates = () => {
-  const [deleteId, setDeleteId] = useState(null);
-  // const { setMessage } = useContext(AlertContext);
-  // const [paginateData, setPaginateData] = useState([]);
-  // const { data, error } = useFetch(Estate.getAll);
-  // const { setSearchData, result } = useContext(SearchContext);
-  // const [deleteId, setDeleteId] = useState(null);
-
-  // const { pageData, currentPage, handleNext, handlePrev, setCurrentPage } =
-  //   usePagination(paginateData);
-
-  // useEffect(() => {
-  //   setCurrentPage(1);
-  // }, [result, setCurrentPage]);
-
-  // useEffect(() => {
-  //   setSearchData(data);
-  //   setMessage(error);
-  //   setPaginateData(result);
-
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, [error, setMessage, data, result, currentPage, setSearchData]);
-
   const headings = ["Logo", "Name", "Code", "Owner", "Actions"];
   const column = [
     (item) => Logo({ url: item.logo }),
