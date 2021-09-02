df773062c4df65d30d34e3698cf3643fdb8c2584
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-09-02 21:06:38 +0100

    Update NavItem.js

--- src/components/navigation/NavItem.js
+++ src/components/navigation/NavItem.js
@@ -2,32 +2,36 @@ import { Link, useLocation } from "react-router-dom";
 import { IconContext } from "react-icons/lib";
 import { useEffect } from "react";
 import { useState } from "react";
-const NavItem = ({ to, name, icon, hide = false }) => {
+import useRoles from "./../../hooks/useRoles";
+const NavItem = ({ to, name, icon, hide = false, rights = [] }) => {
   const location = useLocation();
   const [isActive, setIsActive] = useState(false);
+  const role = useRoles(rights);
 
   useEffect(() => {
     setIsActive(location.pathname === to);
   }, [location, to]);
 
   return (
-    <Link
-      to={to}
-      className={`group px-2 py-1 rounded-md text-xs font-medium tracking-wider text-gray-500 flex items-center justify-start space-x-3 hover:text-gray-900 capitalize hover:bg-gray-200 dark:hover:bg-indigo-900 w-full ${
-        isActive ? "bg-gray-100 dark:bg-indigo-900 dark:text-white" : ""
-      }`}
-    >
-      <span>
-        <IconContext.Provider
-          value={{
-            className: "h-5 w-5 group-hover:text-indigo-500",
-          }}
-        >
-          {icon}
-        </IconContext.Provider>
-      </span>
-      {!hide && <span className="dark:hover:text-gray-200">{name}</span>}
-    </Link>
+    role && (
+      <Link
+        to={to}
+        className={`group px-2 py-1 rounded-md text-xs font-medium tracking-wider text-gray-500 flex items-center justify-start space-x-3 hover:text-gray-900 capitalize hover:bg-gray-200 dark:hover:bg-indigo-900 w-full ${
+          isActive ? "bg-gray-100 dark:bg-indigo-900 dark:text-white" : ""
+        }`}
+      >
+        <span>
+          <IconContext.Provider
+            value={{
+              className: "h-5 w-5 group-hover:text-indigo-500",
+            }}
+          >
+            {icon}
+          </IconContext.Provider>
+        </span>
+        {!hide && <span className="dark:hover:text-gray-200">{name}</span>}
+      </Link>
+    )
   );
 };
 
