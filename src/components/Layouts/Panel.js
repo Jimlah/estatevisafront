7789a841b760399681ce0c1af09c9b9b84245a2c
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-09-02 16:46:42 +0100

    Created modal panel

--- src/components/Layouts/Panel.js
+++ src/components/Layouts/Panel.js
@@ -1,10 +1,27 @@
+import { FaTimes } from "react-icons/fa";
+import { useState } from "react";
+
 const Panel = (props) => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const handleHide = () => {
+    setIsOpen(false);
+  };
+
   return (
-    <div className="absolute top-0 left-0 h-screen w-full flex items-center justify-center px-5 sm:px-10">
-      <div className="bg-gray-100 dark:bg-gray-900 px-5 inline-block rounded-lg py-10">
-        {props.children}
+    isOpen && (
+      <div className="absolute top-0 left-0 h-screen w-full flex items-center justify-center px-5 sm:px-10">
+        <div className="relative bg-gray-100 dark:bg-gray-900 px-5 inline-block rounded-lg py-10">
+          <button
+            onClick={handleHide}
+            className="absolute top-0 right-0 mr-3 text-gray-500"
+          >
+            <FaTimes />
+          </button>
+          {props.children}
+        </div>
       </div>
-    </div>
+    )
   );
 };
 
