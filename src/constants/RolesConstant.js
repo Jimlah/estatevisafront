941446d5b87da8f0cac81df96c91c37852ac6e71
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-09-02 21:07:11 +0100

    Create RolesConstant.js

---
+++ src/constants/RolesConstant.js
@@ -0,0 +1,6 @@
+export const SUPER_ADMIN = "super_admin";
+export const ADMIN = "admin";
+export const ESTATE_OWNER = "estate_owner";
+export const ESTATE_ADMIN = "estate_admin";
+export const HOUSE_OWNER = "house_owner";
+export const HOUSE_SUB_OWNER = "house_sub_owner";
