adf39a51819319704e266519f704238ee62cd299
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-09-02 21:07:03 +0100

    Update Root.js

--- src/components/pages/layout/Root.js
+++ src/components/pages/layout/Root.js
@@ -3,13 +3,32 @@ import Main from "../dashboard/Main";
 import { Switch } from "react-router-dom";
 import PrivateRoute from "../../Routes/PrivateRoute";
 import PublicRoute from "../../Routes/PublicRoute";
+import {
+  SUPER_ADMIN,
+  ADMIN,
+  ESTATE_OWNER,
+  ESTATE_ADMIN,
+  HOUSE_OWNER,
+  HOUSE_SUB_OWNER,
+} from "../../../constants/RolesConstant";
 
 const Root = () => {
   return (
-    <div className="bg-indigo-900 h-screen">
+    <div className="bg-indigo-900 h-screen slashed-zero">
       <Switch>
         <PublicRoute restricted={true} path="/auth" component={BaseAuth} />
-        <PrivateRoute path="/dashboard" component={Main} />
+        <PrivateRoute
+          path="/dashboard"
+          rights={[
+            SUPER_ADMIN,
+            ADMIN,
+            ESTATE_OWNER,
+            ESTATE_ADMIN,
+            HOUSE_OWNER,
+            HOUSE_SUB_OWNER,
+          ]}
+          component={Main}
+        />
       </Switch>
     </div>
   );
