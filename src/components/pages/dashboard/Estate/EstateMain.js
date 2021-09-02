6a6f3bd3c7bd1aba82ac40e370dfcec681af2428
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-09-02 21:06:53 +0100

    Update EstateMain.js

--- src/components/pages/dashboard/Estate/EstateMain.js
+++ src/components/pages/dashboard/Estate/EstateMain.js
@@ -2,16 +2,44 @@ import { Switch } from "react-router-dom";
 import PrivateRoute from "../../../Routes/PrivateRoute";
 import ViewEstates from "./ViewEstates";
 import CreateEstates from "./CreateEstates";
+import {
+  SUPER_ADMIN,
+  ADMIN,
+  ESTATE_OWNER,
+  ESTATE_ADMIN,
+  HOUSE_OWNER,
+  HOUSE_SUB_OWNER,
+} from "../../../../constants/RolesConstant";
 
 const EstateMain = () => {
   return (
     <>
       <Switch>
-        <PrivateRoute path="/dashboard/estates" component={ViewEstates} exact />
+        <PrivateRoute
+          path="/dashboard/estates"
+          rights={[
+            SUPER_ADMIN,
+            ADMIN,
+            ESTATE_OWNER,
+            ESTATE_ADMIN,
+            HOUSE_OWNER,
+            HOUSE_SUB_OWNER,
+          ]}
+          component={ViewEstates}
+          exact
+        />
         <PrivateRoute
           path="/dashboard/estates/create"
           component={CreateEstates}
           exact
+          rights={[
+            SUPER_ADMIN,
+            ADMIN,
+            ESTATE_OWNER,
+            ESTATE_ADMIN,
+            HOUSE_OWNER,
+            HOUSE_SUB_OWNER,
+          ]}
         />
       </Switch>
     </>
