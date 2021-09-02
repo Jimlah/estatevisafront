435dfe6a0d7dbac5acf5282197f46353f3fbc05d
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-09-02 21:06:32 +0100

    modified panel

--- src/components/Layouts/Panel.js
+++ src/components/Layouts/Panel.js
@@ -10,7 +10,7 @@ const Panel = (props) => {
 
   return (
     isOpen && (
-      <div className="absolute top-0 left-0 h-screen w-full flex items-center justify-center px-5 sm:px-10">
+      <div className="absolute top-0 left-0 h-screen w-full flex items-center justify-center px-5 sm:px-10 z-50">
         <div className="relative bg-gray-100 dark:bg-gray-900 px-5 inline-block rounded-lg py-10">
           <button
             onClick={handleHide}
