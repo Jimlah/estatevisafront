23472a4d7ce74e7547312c724640a6ad87580711
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-09-02 16:47:15 +0100

    Designed the email verify warning

--- src/components/notifications/EmailVerifyWarning.js
+++ src/components/notifications/EmailVerifyWarning.js
@@ -1,6 +1,18 @@
 import Panel from "./../Layouts/Panel";
 import mail from "./../../assets/img/mail.png";
+import { useContext } from "react";
+import { AlertContext } from "./../../context/AlertContext";
+import { Auth } from "./../../services/auth.services";
 const EmailVerifyWarning = () => {
+  const { setMessage, setStatus } = useContext(AlertContext);
+
+  const handleResendEmail = async () => {
+    const res = await Auth.resendVerification();
+
+    setStatus(res.status);
+    setMessage(res.message);
+  };
+
   return (
     <Panel>
       <div className="w-full h-full flex flex-col items-center space-y-8">
@@ -14,11 +26,14 @@ const EmailVerifyWarning = () => {
         ></div>
         <p className="text-xs font-light text-center text-gray-500">
           An Email has been sent to your account with a link to verify your
-          account. If you have not recieve the email after a few minutes please
+          account. If you have not receive the email after a few minutes please
           check your span folder.
         </p>
         <div className="flex items-center justify-center space-x-14">
-          <button className="text-xs font-semibold px-4 py-2 bg-indigo-500 hover:border-indigo-500 border hover:text-indigo-500 hover:bg-transparent text-white rounded-lg">
+          <button
+            onClick={handleResendEmail}
+            className="text-xs font-semibold px-4 py-2 bg-indigo-500 hover:border-indigo-500 border hover:text-indigo-500 hover:bg-transparent text-white rounded-lg"
+          >
             Resend Email
           </button>
           <button className="text-xs font-semibold px-4 py-2 border border-indigo-500 text-indigo-500 hover:bg-indigo-500 hover:text-white rounded-lg">
