67bf16798200db16575afa2234f1b5a5ec15d190
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-09-03 07:59:19 +0100

    updated useRoles hook

--- src/hooks/useRoles.js
+++ src/hooks/useRoles.js
@@ -2,16 +2,23 @@ import { useEffect, useState } from "react";
 import { getUser } from "./../utils/common";
 
 const useRoles = (rights) => {
-  const [roles, setRoles] = useState([]);
+  const [roles, setRoles] = useState(getUser()?.user.roles);
   const [result, setResult] = useState(false);
 
   useEffect(() => {
     const isAllowed = (element) => {
       return roles[element] === true;
     };
-    setRoles(getUser()?.user.roles);
-    const resp = rights?.some(isAllowed);
-    setResult(resp);
+
+    if (roles) {
+      const resp = rights?.some(isAllowed);
+      setResult(resp);
+    }
+
+    return () => {
+      setResult(false);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [result, rights]);
 
   return result;
