aa1b49d94b93a6c1f0cfd554836fe9adf9556821
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-09-11 17:59:15 +0100

    Added status to the Estate

--- src/components/pages/dashboard/Estate/ViewEstates.js
+++ src/components/pages/dashboard/Estate/ViewEstates.js
@@ -3,7 +3,7 @@ import Table from "./../../../table/Table";
 import { Estate } from "./../../../../services/estate.services";
 
 const ViewEstates = () => {
-  const headings = ["Name", "Code", "Owner", "Created At", "Actions"];
+  const headings = ["Name", "Code", "Owner", "Status", "Created At", "Actions"];
 
   const NameLogo = (name, url) => {
     return (
@@ -14,10 +14,30 @@ const ViewEstates = () => {
     );
   };
 
+  const status = (status) => {
+    let bgStyle = "bg-green-500";
+    if (status === "active") {
+      bgStyle = "bg-green-500";
+    } else if (status === "suspended") {
+      bgStyle = "bg-red-500";
+    } else if (status === "pending") {
+      bgStyle = "bg-yellow-500";
+    }
+
+    return (
+      <span
+        className={`px-2 py-1 text-white text-xs font-semibold bg-opacity-70 rounded ${bgStyle}`}
+      >
+        Active
+      </span>
+    );
+  };
+
   const column = [
     (item) => NameLogo(item.name, item.logo),
     (item) => item.code,
     (item) => item.user?.profile?.firstname ?? "UnKnown",
+    (item) => status(item.status),
     (item) => item.created_at,
   ];
 
